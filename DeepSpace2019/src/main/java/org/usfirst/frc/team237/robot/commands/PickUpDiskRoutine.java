f1edbd2bd9666f3c199a2cdd63b1acdaa72c5418
hannahkozar <hannahkozar@hotmail.com>
2019-02-09 13:34:06 -0500

    pickUpDisk Routine command

--- DeepSpace2019/src/main/java/org/usfirst/frc/team237/robot/commands/PickUpDiskRoutine.java
+++ DeepSpace2019/src/main/java/org/usfirst/frc/team237/robot/commands/PickUpDiskRoutine.java
@@ -11,8 +11,20 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class PickUpDiskRoutine extends Command 
 {
+  private State currentState;
+  private double time;
+  private enum State
+  {
+    start,
+    slapdown,
+    extend,
+    retract,
+    slapUp,
+    finished
+  };
   public PickUpDiskRoutine() 
   { 
+    requires(Robot.diskHandler);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
@@ -21,28 +33,85 @@ public class PickUpDiskRoutine extends Command
   @Override
   protected void initialize() 
   {
-
+    time = Timer.getFGPATimeStamp;
+    currentState = State.start;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {
+  protected void execute()
+  {
+    switch(currentState)
+    {
+      case start:
+        currentState = State.slapdown;
+        break;
+
+      case slapDown:
+      Robot.diskHandler.diskDown();
+      if(Timer.getFGPATimeStamp + time > 1)
+      {
+        time = Timer.getFGPATimeStamp;
+        currentState = State.extend;
+      }
+      break;
+
+      case extend:
+      Robot.diskHandler.diskEntend();
+      if(Timer.getFGPATimeStamp + time > 1)
+      {
+        time = Timer.getFGPATimeStamp;
+        currentState = State.retract;
+      }
+      break;
+
+      case retract:
+      Robot.diskHandler.diskRetract();
+      if(Timer.getFGPATimeStamp + time > 1)
+      {
+        time = Timer.getFGPATimeStamp;
+        currentState = State.slapUp;
+      }
+      break;
+
+      case slapUp: 
+      Robot.diskHandler.diskUp();
+      if(Timer.getFGPATimeStamp + time > 1)
+      {
+        time = Timer.getFGPATimeStamp;
+        currentState = State.finished;
+      }
+      break;
+
+      default:
+      break;
+    }
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() {
+  protected boolean isFinished() 
+  {
+    if(currentState == State.finished)
+    {
+      return true;
+    }
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
-  protected void end() {
+  protected void end() 
+  {
+
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
-  protected void interrupted() {
+  protected void interrupted() 
+  {
+
   }
 }
